b91cc940eaa37ae2a1492536f45c325847326401
suubinkim <0vbbbv0@gmail.com>
2021-10-25 23:56:13 +0900

    1025 자바 알고리즘

--- section2/Main.java
+++ section2/Main.java
@@ -3,18 +3,31 @@ package com.company.section2;
 import java.util.*;
 
 public class Main {
-    public int[] solution(int t) {
+    public int[] solution(int t,int[] num) {
         int[] answer = new int[t];
-        answer[0] = answer[1] = 1;
-        for(int i=2;i<t;i++) {
-            answer[i] = answer[i-1] + answer[i-2];
+
+        for(int i=0;i<t;i++){
+            int rank = 1;
+            for(int j=0;j<t;j++){
+                if(num[i]<num[j]){
+                    rank++;
+                }
+            }
+            answer[i]=rank;
         }
+
         return answer;
     }
+
+
     public static void main(String[] args) {
         Main T = new Main();
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
-        for (int x : T.solution(t)) System.out.print(x+" ");
+        int[] num = new int[t];
+        for(int i=0;i<t;i++){
+            num[i] = in.nextInt();
+        }
+        for(int x : T.solution(t,num)) System.out.print(x+" ");
     }
 }
