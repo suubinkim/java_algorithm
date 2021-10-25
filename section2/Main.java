5a5eb0bd9ceb59964bd4407653540c3964642f0d
suubinkim <0vbbbv0@gmail.com>
2021-10-25 21:40:53 +0900

    1025 자바 알고리즘

--- section2/Main.java
+++ section2/Main.java
@@ -1,4 +1,20 @@
 package com.company.section2;
 
+import java.util.*;
+
 public class Main {
+    public int[] solution(int t) {
+        int[] answer = new int[t];
+        answer[0] = answer[1] = 1;
+        for(int i=2;i<t;i++) {
+            answer[i] = answer[i-1] + answer[i-2];
+        }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for (int x : T.solution(t)) System.out.print(x+" ");
+    }
 }
