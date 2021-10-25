17ee4c6291218f59b8afa86c9c4e610c84bf56f6
suubinkim <0vbbbv0@gmail.com>
2021-10-25 23:57:19 +0900

    1025 06_뒤집은 소수

--- section2/Main.java
+++ section2/Main.java
@@ -3,17 +3,24 @@ package com.company.section2;
 import java.util.*;
 
 public class Main {
-    public int[] solution(int t,int[] num) {
-        int[] answer = new int[t];
+    public boolean isPrime(int num) {
+        if(num==1) return false;
+        for(int i=2;i<num;i++) {
+            if(num%i==0) return false;
+        }
+        return true;
+    }
 
-        for(int i=0;i<t;i++){
-            int rank = 1;
-            for(int j=0;j<t;j++){
-                if(num[i]<num[j]){
-                    rank++;
-                }
+    public ArrayList<Integer> solution(int t,int[] num) {
+        ArrayList<Integer> answer= new ArrayList<>();
+        for(int i=0;i<t;i++) {
+            int rev = 0;
+            while (num[i]>0) {
+                int digit =  num[i] % 10;
+                rev = rev * 10 + digit;
+                num[i] = num[i]/10;
             }
-            answer[i]=rank;
+            if(isPrime(rev)) answer.add(rev);
         }
 
         return answer;
@@ -25,8 +32,8 @@ public class Main {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         int[] num = new int[t];
-        for(int i=0;i<t;i++){
-            num[i] = in.nextInt();
+        for(int i=0;i<t;i++) {
+            num[i]=in.nextInt();
         }
         for(int x : T.solution(t,num)) System.out.print(x+" ");
     }
