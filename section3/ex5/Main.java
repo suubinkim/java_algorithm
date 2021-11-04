a58f8d88e6d8707a85818d281824d57c2a35f9d9
suubinkim <0vbbbv0@gmail.com>
2021-11-04 22:29:26 +0900

    섹션3 6

--- section3/ex5/Main.java
+++ section3/ex5/Main.java
@@ -4,23 +4,20 @@ import java.util.*;
 public class Main {
 
     public int solution(int n) {
-        int answer=0, sum = 0,lt=0;
-        int[] arr = new int[n];
-        for(int i=0;i<n;i++){
+        int answer=0, sum = 0;
+        int lt=0;
+        int[] arr = new int[n/2+1];
+        for(int i=0;i<n/2+1;i++){
             arr[i]=i+1;
         }
-        for(int i=0;i<n;i++){
-
-            while(sum<=n){
-                sum+= arr[i];
+        for(int rt=0;rt<n/2+1;rt++){
+            sum += arr[rt];
+            if(sum==n) answer++;
+            while(sum>=n){
+                sum-=arr[lt++];
                 if(sum==n)answer++;
             }
-            if(sum>n){
-                sum -= arr[lt++];
-                if(sum==n) answer++;
-            }
         }
-
         return answer;
     }
     public static void main(String[] args) {
