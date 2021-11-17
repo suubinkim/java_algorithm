8d1f0b3a4e7a47b07b8f42142bd078bd1b79d24e
suubinkim <0vbbbv0@gmail.com>
2021-11-17 22:16:00 +0900

    섹션4-5

--- section4/ex04/Main.java
+++ section4/ex04/Main.java
@@ -7,31 +7,27 @@ import java.util.Scanner;
 public class Main {
     public int solution(String s, String t) {
         int answer =0;
-        int lt=0, rt=t.length()-1;
-        int cnt = 0;
-        char[] ss = s.toCharArray();
-        char[] tt = t.toCharArray();
-        HashMap<Character,Integer> map = new HashMap<>();
-        for(int i=0;i<t.length();i++){
-            map.put(ss[i],map.getOrDefault(ss[i],0)+1);
+        int lt=0;
+        char[] arr = s.toCharArray();
+        HashMap<Character,Integer> ss = new HashMap<>();
+        HashMap<Character,Integer> tt = new HashMap<>();
+
+        for(int i=0;i<t.length()-1;i++){
+            ss.put(arr[i],ss.getOrDefault(arr[i],0)+1);
         }
-        while(rt<s.length()){
-            for(char x : tt){
-                if(map.containsKey(x)){
-                    if(map.get(x)==1)cnt++;
-                }else break;
-            }
-            if(cnt==t.length())answer++;
-            rt++;
-            if(rt==s.length())break;
-            map.put(ss[rt],map.getOrDefault(ss[rt],0)+1);
-            map.put(ss[lt],map.get(ss[lt])-1);
-            if(map.get(ss[lt])==0){
-                map.remove(ss[lt]);
-            }
-            cnt=0;
+
+        for(char x : t.toCharArray()){
+            tt.put(x,tt.getOrDefault(x,0)+1);
+        }
+
+        for(int rt=t.length()-1;rt<s.length();rt++){;
+            ss.put(arr[rt],ss.getOrDefault(arr[rt],0)+1);
+            if(ss.equals(tt))answer++;
+            ss.put(arr[lt],ss.getOrDefault(arr[lt],0)-1);
+            if(ss.get(arr[lt])==0)ss.remove(arr[lt]);
             lt++;
         }
+
 
         return answer;
     }
