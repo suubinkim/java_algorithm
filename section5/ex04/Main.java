07b1fc930c26f5c24c737ef005d6d161a389545d
suubinkim <0vbbbv0@gmail.com>
2021-11-22 22:39:30 +0900

    섹션5-3

---
+++ section5/ex04/Main.java
@@ -0,0 +1,18 @@
+package com.company.section5.ex04;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class Main {
+    public int solution(String t ) {
+        int answer=0;
+
+        return answer;
+    }
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        String t = in.next();
+        System.out.print(T.solution(t));
+    }
+}
