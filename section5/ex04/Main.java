7369f033441c716410fc77df2600b72feedb40f8
suubinkim <0vbbbv0@gmail.com>
2021-11-30 22:35:20 +0900

    섹션5-4,5

--- section5/ex04/Main.java
+++ section5/ex04/Main.java
@@ -6,6 +6,20 @@ import java.util.Stack;
 public class Main {
     public int solution(String t ) {
         int answer=0;
+        Stack<Integer> stack = new Stack<>();
+        for(char x : t.toCharArray()){
+            if(Character.isDigit(x))stack.push(x-48);
+            else {
+                int rt = stack.pop();
+                int lt = stack.pop();
+                if(x=='+')stack.push(lt+rt);
+                else if (x=='-')stack.push(lt-rt);
+                else if (x=='*')stack.push(lt*rt);
+                else if (x=='/')stack.push(lt/rt);
+            }
+        }
+        answer = stack.pop();
+
 
         return answer;
     }
