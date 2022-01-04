feedef9f17af4f4c1d925eea63b6e41573575b20
suubinkim <0vbbbv0@gmail.com>
2022-01-04 20:37:24 +0900

    섹션6 선택정렬 수정

--- section6/ex01/Main.java
+++ section6/ex01/Main.java
@@ -4,22 +4,20 @@ import java.util.Scanner;
 
 public class Main {
     public int[] solution(int n, int[] arr) {
-        int[] answer = new int[n];
         int min;
         int temp;
-        for (int i = 0; i < arr.length; i++) {
+        for (int i = 0; i < arr.length-1; i++) {
             min = i;
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[min] > arr[j]) {
                     min = j;
                 }
             }
-            answer[i] = arr[min];
             temp = arr[min];
             arr[min] = arr[i];
             arr[i] = temp;
         }
-        return answer;
+        return arr;
     }
 
     public static void main(String[] args) {
