1b9acf56c39ee853966baf84674874b851762788
suubinkim <0vbbbv0@gmail.com>
2022-01-06 21:29:53 +0900

    섹션6 버블정렬

---
+++ section6/ex02/Main.java
@@ -0,0 +1,32 @@
+package com.company.section6.ex02;
+
+import java.util.Scanner;
+
+public class Main {
+    public int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+
+    }
+}
