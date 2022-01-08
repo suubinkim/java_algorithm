598577692f7be39005afc2a48818dc5986cf7e98
suubinkim <0vbbbv0@gmail.com>
2022-01-08 21:40:08 +0900

    섹션6 삽입정렬

---
+++ section6/ex03/Main.java
@@ -0,0 +1,31 @@
+package com.company.section6.ex03;
+
+import java.util.Scanner;
+
+public class Main {
+    public int[] solution(int n, int[] arr) {
+        for (int i = 1; i < n; i++) {
+            int target = arr[i];
+            int j = i - 1;
+            while (j >= 0 && target < arr[j]) {
+                arr[j + 1] = arr[j];
+                j--;
+            }
+            arr[j + 1] = target;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = in.nextInt();
+        }
+        for (int x : T.solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
