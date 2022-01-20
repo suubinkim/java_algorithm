feda8be3a5cc4aad2fe6ceabcf18f85ee4d308af
suubinkim <0vbbbv0@gmail.com>
2022-01-20 15:03:56 +0900

    섹션6 이분검색 (성공)

--- section6/ex08/Main.java
+++ section6/ex08/Main.java
@@ -7,8 +7,16 @@ public class Main {
     public int solution(int n, int m, int[] arr) {
         int answer = 0;
         Arrays.sort(arr);
-        for (int i = 0; i < n; i++) {
-            if (arr[i] == m) answer = i + 1;
+        int lt = 0;
+        int rt = n - 1;
+        while (lt <= rt) {
+            int mid = (lt + rt) / 2;
+            if (arr[mid] == m) {
+                answer = mid + 1;
+                break;
+            } else if (arr[mid] > m) {
+                rt = mid - 1;
+            } else lt = mid + 1;
         }
         return answer;
     }
