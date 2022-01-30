2b5abd22673a625f34efa4f33b295a91a87016cf
suubinkim <0vbbbv0@gmail.com>
2022-01-30 22:53:39 +0900

    섹션7 재귀함수 ( 다시 )

---
+++ section7/ex01/Main.java
@@ -0,0 +1,21 @@
+package com.company.section7.ex01;
+
+public class Main {
+    public void DFS(int n) {
+        if (n == 0) return;
+        else {
+            DFS(n - 1);
+            System.out.print(n + " ");
+        }
+    }
+
+    public void solution(int n) {
+        DFS(n);
+    }
+
+    public static void main(String[] args) {
+        Main T = new Main();
+        T.solution(3);
+        System.out.println(T.solution(3));
+    }
+}
