5741e722a0d2258694390a522822547908c9da6e
suubinkim <0vbbbv0@gmail.com>
2022-02-26 11:08:46 +0900

    섹션7 피보나치 재귀2

--- section7/ex04/Main.java
+++ section7/ex04/Main.java
@@ -2,20 +2,23 @@ package com.company.section7.ex04;
 
 import java.util.Scanner;
 
-public class Main {
+class Main {
+    static int[] fibo;
+
     public int solution(int n) {
-        if(n==1) return 1;
-        else if(n==2) return 1;
-        else return solution(n-2) + solution(n-1);
+        if (fibo[n] > 0) return fibo[n];
+        if (n == 1) return fibo[n] = 1;
+        else if (n == 2) return fibo[n] = 1;
+        else return fibo[n] = solution(n - 2) + solution(n - 1);
     }
 
     public static void main(String[] args) {
         Main T = new Main();
         Scanner in = new Scanner(System.in);
         int n = in.nextInt();
-        for (int i = 1; i <= n; i++) {
-            System.out.print(T.solution(i) + " ");
-        }
-
+        fibo = new int[n + 1];
+        T.solution(n);
+        for (int i = 1; i <= n; i++) System.out.print(fibo[i] + " ");
     }
+
 }
