36c372371d99cd8c1dc965ab19e61fd4157e4452
suubinkim <0vbbbv0@gmail.com>
2022-03-28 19:55:53 +0900

    tree 말단노드까지의 가장 짧은 경로 (BFS)

--- inflearn/section7/ex09/Main.java
+++ inflearn/section7/ex09/Main.java
@@ -1,6 +1,9 @@
 package com.company.inflearn.section7.ex09;
 
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 class Node {
     int data;
     Node lt, rt;
@@ -14,9 +17,23 @@ class Node {
 public class Main {
     Node root;
 
-    public int DFS(int L, Node root) {
-        if (root.lt == null && root.rt == null) return L;
-        else return Math.min(DFS(L + 1, root.lt), DFS(L + 1, root.rt));
+    public int BFS(Node root) {
+        Queue<Node> Q = new LinkedList<>();
+        Q.offer(root);
+        int L = 0;
+        while (!Q.isEmpty()) {
+            int len = Q.size();
+            for (int i = 0; i < len; i++) {
+                //현재 노드 == 꺼낸 root
+                Node cur = Q.poll();
+                //자식 노드 있는지 확인
+                if (cur.lt == null && cur.rt == null) return L;
+                if (cur.lt != null) Q.offer(cur.lt);
+                if (cur.rt != null) Q.offer(cur.rt);
+            }
+            L++;
+        }
+        return 0;
     }
 
     public static void main(String[] args) {
@@ -26,6 +43,6 @@ public class Main {
         tree.root.rt = new Node(3);
         tree.root.lt.lt = new Node(4);
         tree.root.lt.rt = new Node(5);
-        System.out.println(tree.DFS(0, tree.root));
+        System.out.println(tree.BFS(tree.root));
     }
 }
